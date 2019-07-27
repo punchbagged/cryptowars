42c25b67f63f2ae84281fe619609528f685dc50a
punchbagged <daniel@Daniels-MBP.lan>
2019-07-27 16:57:47 +0800

    instant load images

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
+var folder = "images/";
+$.ajax({
+    url : folder,
+    success: function (data) {
+        $(data).find("a").attr("href", function (i, val) {
+            if( val.match(/\.(jpe?g|png|gif)$/) ) { 
+                $("#loadImgs").append( "<img style='display:none;height:1px;width:1px' src='"+ folder + val +"'>" );
+            } 
+        });
+    }
+});
 var statusTxtTimer;
 $("#statusTxtSpan").on("change",function(){
 	$(".statusDiv").animate({borderColor: "rgb(255, 215, 0)"},500);
